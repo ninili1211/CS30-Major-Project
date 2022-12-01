dac1242049d8069c50a5cfc6b6fe88c9b271b21f
Li1302345 <1302345@sps-student.sk.ca>
2022-12-01 12:17:34 -0600

    added start page background

--- sketch.js
+++ sketch.js
@@ -2,11 +2,20 @@
 // Nini Li
 // 11/22/2022
 
+//Start Page
+let startPageImage;
+let scalar = 2;
+
+function preload() {
+  startPageImage = loadImage("startPageBackground.jpg");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
+  image(startPageImage, windowWidth/2, windowHeight/4, startPageImage.width * scalar, startPageImage.height * 2);
 }
-
