ceb5b1a20bbe9b6aee02d4420a80a321ea145529
Li1302345 <1302345@sps-student.sk.ca>
2022-12-06 12:03:51 -0600

    start screen (incorporated, unfinished)

--- sketch.js
+++ sketch.js
@@ -5,9 +5,12 @@
 //Start Page
 let startPageImage;
 let scalar = 2;
+let state = "start";
+let treesImg;
 
 function preload() {
   startPageImage = loadImage("startPageBackground.jpg");
+  treesImg = loadImage("fall-trees.jpg");
 }
 
 function setup() {
@@ -18,4 +21,34 @@ function setup() {
 function draw() {
   background(220);
   image(startPageImage, windowWidth/2, windowHeight/4, startPageImage.width * scalar, startPageImage.height * 2);
+  if (state === "start") {
+    startScreen();
+  }
+  if (state === "main") {
+    image(treesImg, 0, 0, width, height);
+  }
+}
+
+function mousePressed() {
+  if (state === "start" && mouseInsideRect(400, 700, 400, 550)) {
+    state = "main";
+  } 
+}
+
+function startScreen() {
+  if (mouseInsideRect(400, 700, 400, 550)) {
+    fill("gray");
+  }
+  else {
+    fill("black");
+  }
+  rect(400, 400, 300, 150);
+  fill("white");
+  textSize(50);
+  text("Begin!", 480, 490);
+}
+
+function mouseInsideRect(left, right, top, bottom) {
+  return mouseX >= left && mouseX <= right &&
+         mouseY >= top && mouseY <= bottom;
 }
