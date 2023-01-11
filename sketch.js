aa7d25824b2c26413c087bcf3d6aa40b1601b8d2
Li1302345 <1302345@sps-student.sk.ca>
2023-01-11 12:07:02 -0600

    start button + mouse hover

--- sketch.js
+++ sketch.js
@@ -3,50 +3,54 @@
 // 11/22/2022
 
 // //Start Page
-// let state = "start";
-// let startBackImg;
-// let optionBackImg;
-// let startButtonImg;
-// let cursorImg;
+let state = "start";
+let startBackImg;
+let optionBackImg;
+let startButtonImg;
+let startButtonHoverImg;
+let cursorImg;
 
-// function preload() {
-//   startBackImg = loadImage("startPageBackground.jpg");
-//   optionBackImg = loadImage("optionsBackground.jpg");
-//   startButtonImg = loadImage("startButton.png");
-//   cursorImg = loadImage("cursor.png");
-// }
+function preload() {
+  startBackImg = loadImage("startPageBackground.jpg");
+  optionBackImg = loadImage("optionsBackground.jpg");
+  startButtonImg = loadImage("startButton.png");
+  startButtonHoverImg = loadImage("startButtonHover.png");
+  cursorImg = loadImage("cursor.png");
+}
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
-// function draw() {
-//   background(220);
-//   if (state === "start") {
-//     startScreen();
-//   }
-//   if (state === "main") {
-//     image(optionBackImg, 0, 0, width, height);
-//   }
-// }
+function draw() {
+  background(220);
+  if (state === "start") {
+    startScreen();
+  }
+  if (state === "main") {
+    image(optionBackImg, 0, 0, width, height);
+  }
+}
 
-// function startScreen() {
-//   imageMode(CORNER);
-//   image(startBackImg, 0, 0, width, height);
-//   image(startButtonImg, windowWidth * 0.33, windowHeight * 0.65, width * 0.4, height * 0.4);
-//   fill("#463F3A");
-//   textSize(50);
-//   textFont("Times New Roman");
-//   text("START GAME!!", width * 0.4, height * 0.87);
-// }
+function startScreen() {
+  imageMode(CORNER);
+  image(startBackImg, 0, 0, width, height);
+  image(startButtonImg, windowWidth * 0.33, windowHeight * 0.75, width * 0.35, height * 0.2);
+}
 
-// function startState() {
-//   if (state === "start") {
-//     startScreen();
-//   }
-//   if (state === "main") {
-//     image(optionBackImg, 0, 0, width, height);
-//   }
+function startState() {
+  if (state === "start") {
+    startScreen();
+  }
+  if (state === "main") {
+    image(optionBackImg, 0, 0, width, height);
+  }
+}
+
+// function mouseTouching() {
+//     if (mouseX && mouseY === ) {
+//         image(startButtonImg,)
+//     }
 // }
 
 // //tic tac toe
