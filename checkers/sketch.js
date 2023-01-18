77454c1ac69215d775b0b394f506c6809057eb1d
Li1302345 <1302345@sps-student.sk.ca>
2023-01-18 12:06:08 -0600

    elegantifying checkers ;p

--- checkers/sketch.js
+++ checkers/sketch.js
@@ -1,28 +1,26 @@
+//variables 
 const cols = 8;
 const rows = 8;
 const board = Array(rows).fill().map(() => Array(cols).fill(0));
-
 const w = 60;
 const dw = 48;
-
 const SELECT_STATE = 0;
 const MOVE_STATE = 1;
 let state = SELECT_STATE;
-
 let selectedI = -1;
 let selectedJ = -1;
-
 const BLUE = 1;
 const RED = 2;
 const WHITE = 1;
 const BLACK = 2;
 let player = BLUE;
 
+//setup
 function setup() {
   createCanvas(cols*w, rows*w);
   for (let j = 0; j < 3; j++) {
     for (let i = 0; i < cols; i++) {
-      if (i % 2 != j % 2) {
+      if (i % 2 !== j % 2) {
         board[j][i] = 2;
       }
     }
@@ -30,7 +28,7 @@ function setup() {
   
   for (let j = rows-3; j < rows; j++) {
     for (let i = 0; i < cols; i++) {
-      if (i % 2 != j % 2) {
+      if (i % 2 !== j % 2) {
         board[j][i] = 1;
       }
     }
@@ -43,18 +41,19 @@ function draw() {
     for (let i = 0; i < cols; i++) {
       noStroke();
       fill(51);
-      if (i % 2 == j % 2) {
+      if (i % 2 === j % 2) {
         fill(204);
       }
       rect(i*w, j*w, w, w);
       
       if (board[j][i] > 0) {
-        if (board[j][i] == 1) {
+        if (board[j][i] === 1) {
           fill(0, 0, 255);
-        } else if (board[j][i] == 2) {
+        } 
+        else if (board[j][i] === 2) {
           fill(255, 0, 0);
         }
-        if (i == selectedI && j == selectedJ) {
+        if (i === selectedI && j === selectedJ) {
           stroke(255, 255, 0);
         }
         circle(i*w + w/2, j*w + w/2, dw);
@@ -63,29 +62,36 @@ function draw() {
   }
 }
 
+function grid() {
+  
+}
+
 function mousePressed() {
   const i = floor(mouseX / w);
   const j = floor(mouseY / w);
-  if (state == SELECT_STATE) {
-    if (board[j][i] == player) {
+  if (state === SELECT_STATE) {
+    if (board[j][i] === player) {
       selectedI = i;
       selectedJ = j;
     }
     state = MOVE_STATE;
-  } else if (state == MOVE_STATE) {
-    if (board[j][i] == player) {
+  } 
+  else if (state === MOVE_STATE) {
+    if (board[j][i] === player) {
       selectedI = i;
       selectedJ = j;
-    } else if (board[j][i] == 0) {
-      if (player == 1) {
-        if ((i == selectedI-1 || i == selectedI+1) && j == selectedJ-1) {
+    } 
+    else if (board[j][i] === 0) {
+      if (player === 1) {
+        if ((i === selectedI-1 || i === selectedI+1) && j === selectedJ-1) {
           board[j][i] = player;
           board[selectedJ][selectedI] = 0;
           player = 2;
           state = SELECT_STATE;
         }
-      } else if (player == 2) {
-        if ((i == selectedI-1 || i == selectedI+1) && j == selectedJ+1) {
+      } 
+      else if (player === 2) {
+        if ((i === selectedI-1 || i === selectedI+1) && j === selectedJ+1) {
           board[j][i] = player;
           board[selectedJ][selectedI] = 0;
           player = 1;
